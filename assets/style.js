5f55922a2e4ea760a9ab43d97ef283ade04854cb
Tyler Buck <57814333+thetylerb@users.noreply.github.com>
2020-05-18 19:32:29 -0400

    Main HTML
    Added the login page and the basic page for the main.html

---
+++ assets/style.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+  
+});
