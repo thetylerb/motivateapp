5cc19d3143276c6e91b6e1194bdfc685e0edd68c
Tyler Buck <57814333+thetylerb@users.noreply.github.com>
2020-05-19 17:37:22 -0400

    additions to main/style

--- assets/style.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
-  
-});
---
+++ style.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  M.AutoInit();
+  $('.parallax').parallax()
+});
